ae31aa97825961a39dd036700c0ccf3ac90b8c48
Moataz-AbdElKhalek <2668784+Moataz-AbdElKhalek@users.noreply.github.com>
2021-04-26 18:45:58 -0500

    Update buffer_overflow1.c

--- pwnables/buffer overflow/buffer_overflow1.c
+++ pwnables/buffer overflow/buffer_overflow1.c
@@ -3,13 +3,10 @@
 #include <unistd.h>
 int main() {
    int password = 0xcafebabe;
-   printf("The password is: %d\n", password);
    char your_name[100] = {0};
    printf("Enter your name\n");
    fflush(stdout);
    read(0, your_name, 0x100);
-   printf("The password is: %d\n", password);
-   printf("It should be: %d\n", 0xdeadbeef);
 
    if(password == 0xdeadbeef) {
        puts("GOOD WORK!\n");
